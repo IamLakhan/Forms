93fbef4ffeb5ece79cfeee381a97b718ab73b7be
IamLakhan <gurbanilakhan7@gmail.com>
2021-08-13 21:17:49 +0530

    Im so tired, just mundane temperory updates

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('forms/', form_general),
+    path('', greet)
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 def form_general(request):
-    pass
+    return HttpResponse('Hello')
+
+def greet(request):
+    return HttpResponse('Hello {0}'.format(request.user))
